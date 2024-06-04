d962e32e3bbd848c90bd807ee30a4d3e61a90d35
jlopezh73 <jlopezh73@gmail.com>
2024-06-03 18:49:26 -0600

    Primera Version

--- src/main/java/ejemplos2024/clientechat2024/ui/ContenedorCliente.java
+++ src/main/java/ejemplos2024/clientechat2024/ui/ContenedorCliente.java
@@ -1,9 +1,23 @@
 package ejemplos2024.clientechat2024.ui;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import ejemplos2024.clientechat2024.dto.InicioSesionDTO;
+import ejemplos2024.clientechat2024.dto.MensajeDTO;
+import ejemplos2024.clientechat2024.dto.RespuestaInicioSesionDTO;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 public class ContenedorCliente extends BorderPane {
     private TitledPane paneConexion;
@@ -22,9 +36,24 @@ public class ContenedorCliente extends BorderPane {
     private TextField txtMensaje;
     private Button btnConectar;
     private Button btnEnviarMensaje;
+    private Socket servidor;
+    private BufferedReader entrada;
+    private BufferedWriter salida;
+    private Type tipoMensaje;
+    private Type tipoRespuestaInicioSesion;
+    private Gson gson;
+    private final String FORMATO_FECHA = "dd/MM/yyyy";
+    private boolean dejarEscuchar = true;
+    private RespuestaInicioSesionDTO respuesta;
     public ContenedorCliente() {
         crearComponentes();
+        tipoMensaje = new TypeToken<MensajeDTO>(){}.getType();
+        tipoRespuestaInicioSesion = new TypeToken<RespuestaInicioSesionDTO>(){}.getType();
+        gson = new GsonBuilder()
+                .setDateFormat(FORMATO_FECHA)
+                .create();
     }
+
 
     private void crearComponentes() {
         //Crear Panel de Conexión
@@ -45,6 +74,12 @@ public class ContenedorCliente extends BorderPane {
         txtPassword = new PasswordField();
         txtPassword.setText("alfaomega");
         btnConectar = new Button("Conectar");
+        btnConectar.setOnAction(evt -> {
+            if (btnConectar.getText().equals("Conectar"))
+                conectarServidor();
+            else
+                desconectarse();
+        });
 
         HBox contPaneConexion = new HBox();
         contPaneConexion.getChildren().addAll(etiServidor, txtServidor, etiPuerto, txtPuerto,
@@ -63,6 +98,9 @@ public class ContenedorCliente extends BorderPane {
         txtMensaje.setPromptText("Escriba su mensaje a enviar...");
         txtMensaje.setMinWidth(900);
         btnEnviarMensaje = new Button("Enviar");
+        btnEnviarMensaje.setOnAction(evt -> {
+            enviarMensaje();
+        });
 
         setCenter(paneMensajes);
 
@@ -71,5 +109,115 @@ public class ContenedorCliente extends BorderPane {
         paneMensaje.setContent(contPaneMensaje);
 
         setBottom(paneMensaje);
+        habilitar(false);
+    }
+
+    private void desconectarse() {
+        try {
+            dejarEscuchar = true;
+            habilitar(false);
+            btnConectar.setText("Conectar");
+            entrada.close();
+            salida.close();
+            servidor.close();
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void habilitar(boolean hab) {
+        txtServidor.setDisable(hab);
+        txtPuerto.setDisable(hab);
+        txtUsuario.setDisable(hab);
+        txtPassword.setDisable(hab);
+
+        txtMensaje.setDisable(!hab);
+        btnEnviarMensaje.setDisable(!hab);
+    }
+
+    private void enviarMensaje() {
+        String sMensaje = txtMensaje.getText();
+        if (sMensaje.trim().length() > 0) {
+            try {
+                MensajeDTO mensaje = new MensajeDTO();
+                mensaje.setIdUsuario(respuesta.getIdUsuario());
+                mensaje.setUsuario(respuesta.getNombre());
+                mensaje.setMensaje(sMensaje);
+
+                String mensajeJSON = gson.toJson(mensaje);
+                salida.write(mensajeJSON + "\n");
+                salida.flush();
+
+                txtMensaje.setText("");
+                txtMensaje.requestFocus();
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+    private void conectarServidor() {
+        try {
+            String direccionServidor = txtServidor.getText();
+            int puerto = Integer.parseInt(txtPuerto.getText());
+            servidor = new Socket();
+            servidor.connect(new InetSocketAddress(direccionServidor, puerto));
+            entrada = new BufferedReader(
+                    new InputStreamReader(servidor.getInputStream()));
+            salida = new BufferedWriter(
+                    new OutputStreamWriter(servidor.getOutputStream()));
+
+            String usuario = txtUsuario.getText();
+            String password = txtPassword.getText();
+            InicioSesionDTO inicioSesion = new InicioSesionDTO();
+            inicioSesion.setCorreoElectronico(usuario);
+            inicioSesion.setPassword(password);
+            String cadInicioSesion=gson.toJson(inicioSesion);
+
+            salida.write(cadInicioSesion+"\n");
+            salida.flush();
+
+            String respuestaJSON = entrada.readLine();
+            respuesta = gson.fromJson(respuestaJSON,
+                      tipoRespuestaInicioSesion);
+            if (respuesta.isCorrecto()) {
+                dejarEscuchar = false;
+                HiloEscucha hilo = new HiloEscucha();
+                hilo.start();
+
+                habilitar(true);
+                btnConectar.setText("Desconectar...");
+            } else {
+                Alert alerta = new Alert(Alert.AlertType.ERROR);
+                alerta.setTitle("Error");
+                alerta.setHeaderText("Datos de conexión erroenos");
+                alerta.setContentText("El usuario o la contraseña proporcionada son inválidos");
+                alerta.show();
+            }
+
+
+        } catch (Exception e) {
+            Alert alerta = new Alert(Alert.AlertType.ERROR);
+            alerta.setTitle("Error");
+            alerta.setHeaderText("Imposible conectarse al servidor");
+            alerta.setContentText(e.toString());
+            alerta.show();
+        }
+    }
+
+    class HiloEscucha extends Thread {
+        public void run() {
+            while(!dejarEscuchar) {
+                try {
+                    String mensajeJSON = entrada.readLine();
+                    MensajeDTO mensaje = gson.fromJson(mensajeJSON,
+                            tipoMensaje);
+                    txtMensajes.appendText(mensaje.getUsuario()+"> "+mensaje.getMensaje()+"\n");
+                } catch (Exception e) {
+
+                }
+            }
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module ejemplos2024.clientechat2024 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
 
     opens ejemplos2024.clientechat2024 to javafx.fxml;
+    opens ejemplos2024.clientechat2024.dto to com.google.gson;
     exports ejemplos2024.clientechat2024;
 }
